288bce4451be303406234bddb47ceac3c9204e75
Jacek Mleczek <jaccek.m@10g.pl>
2016-06-24 09:41:37 +0200

    Empty Android project

---
+++ app/src/main/java/com/github/jaccek/weatherapp/SplashScreenActivity.java
@@ -0,0 +1,13 @@
+package com.github.jaccek.weatherapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
